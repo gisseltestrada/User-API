72be662b284e6f2a59b882d0ff1da44cf7111d52
gisselt <84683137+gisseltestrada@users.noreply.github.com>
2022-06-08 19:50:26 -0500

    created API calls

--- src/api/controllers/Users/users.controller.ts
+++ src/api/controllers/Users/users.controller.ts
@@ -8,7 +8,7 @@ export async function getUserByEmail(req: Request, res: Response) {
   try {
     const email = req.query.email as string;
     console.log({
-      location: "users.controller",
+      location: "users.controller.getUserByEmail",
       info: `Got request ${JSON.stringify(req.query)}`,
     });
     await client.start();
@@ -16,6 +16,142 @@ export async function getUserByEmail(req: Request, res: Response) {
     if (result) {
       res.status(200).send({
         message: "User retrieved successfully.",
+        success: true,
+        user: result,
+      });
+    } else {
+      res.status(404).send({
+        message: `User with email "${email}" was not found.`,
+        success: false,
+      });
+    }
+  } catch (error: unknown) {
+    res.status(500).send({
+      message: "A server side error ocurred. Please try again.",
+      success: false,
+      erorr: error,
+      query: req.query,
+    });
+  } finally {
+    await client.stop();
+  }
+}
+
+export async function getSalariesByJob(req: Request, res: Response) {
+  const client = new UsersDatabase(collectionName);
+  try {
+    const occupancy = req.query.occupancy as string;
+    console.log({
+      location: "users.controller.getSalariesByJob",
+      info: `Got request ${JSON.stringify(req.query)}`,
+    });
+    await client.start();
+    const result = await client.getSalariesByJob(occupancy);
+    if (result) {
+      res.status(200).send({
+        message: "Users retrieved successfully.",
+        success: true,
+        user: result,
+      });
+    } else {
+      res.status(404).send({
+        message: `Salaries with job title "${occupancy}" were not found.`,
+        success: false,
+      });
+    }
+  } catch (error: unknown) {
+    res.status(500).send({
+      message: "A server side error ocurred. Please try again.",
+      success: false,
+      erorr: error,
+      query: req.query,
+    });
+  } finally {
+    await client.stop();
+  }
+}
+
+export async function createNewUser(req: Request, res: Response) {
+  const client = new UsersDatabase(collectionName);
+  try {
+    const newUser= req.query as object;
+    console.log({
+      location: "users.controller",
+      info: `Got request ${JSON.stringify(req.query)}`,
+    });
+    await client.start();
+    const result = await client.createNewUser(newUser);
+    if (result) {
+      res.status(200).send({
+        message: `Inserted document with _id: ${result.insertedId}`, 
+        success: true,
+        user: result,
+      });
+    } else {
+      res.status(404).send({
+        message: `User could not be created`,
+        success: false,
+      });
+    }
+  } catch (error: unknown) {
+    res.status(500).send({
+      message: "A server side error ocurred. Please try again.",
+      success: false,
+      erorr: error,
+      query: req.query,
+    });
+  } finally {
+    await client.stop();
+  }
+}
+
+export async function updateUser(req: Request, res: Response) {
+  const client = new UsersDatabase(collectionName);
+  try {
+    const updatedUser = req.query as object;
+    console.log({
+      location: "users.controller",
+      info: `Got request ${JSON.stringify(req.query)}`,
+    });
+    await client.start();
+    const result = await client.updateUser(updatedUser);
+    if (result) {
+      res.status(200).send({
+        message: "User updated successfully.",
+        success: true,
+        user: result,
+      });
+    } else {
+      res.status(404).send({
+        message: `User with email "${req.query.email}" was not found.`,
+        success: false,
+      });
+    }
+  } catch (error: unknown) {
+    res.status(500).send({
+      message: "A server side error ocurred. Please try again.",
+      success: false,
+      erorr: error,
+      query: req.query,
+    });
+  } finally {
+    await client.stop();
+  }
+}
+
+export async function deleteUser(req: Request, res: Response) {
+  const client = new UsersDatabase(collectionName);
+  try {
+    const email = req.query.email as string;
+    console.log({
+      location: "users.controller",
+      info: `Got request ${JSON.stringify(req.query)}`,
+    });
+    await client.start();
+    const result = await client.deleteUser(email);
+    if (result) {
+      res.status(200).send({
+        message: "User deleted successfully.",
         success: true,
         user: result,
       });
