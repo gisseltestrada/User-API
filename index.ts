2c192e6d6d53f2eb1d89ba140ec221f9303e751b
ServandoL <swyswy214@gmail.com>
2022-06-22 20:48:15 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import { userRouter } from "./src/api/routes/users";
+import express from 'express';
+import { userRouter } from './src/api/routes/users';
 
 const app = express();
 
-const port: string = process.env.port || "4200";
+const port: string = process.env.port || '4200';
 
 app.use(express.json());
 
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/users', userRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
